6e61a0ce3979b2d1c1888b58fa08e4b0d16ff61e
muhamednasser99 <mn265944@gmail.com>
2025-09-09 22:41:36 +0300

    Add files via upload

---
+++ get-docker.sh
@@ -0,0 +1,4 @@
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl --now enable docker
+sudo usermod -aG docker ansible
